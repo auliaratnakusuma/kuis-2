7e11a87b3370aa54f4e9cb9e8f25aab97455830e
auliaratnakusuma <aulia.ratna.k@mail.ugm.ac.id>
2014-12-03 08:33:00 +0700

    add

--- client.php
+++ client.php
@@ -1,17 +1,13 @@
+
 <?php
 error_reporting(E_ALL);
 ini_set('display_error',1);
 session_start();
 require_once('nusoap/lib/nusoap.php');
-$url = 'http://api.radioreference.com/soap2/index.php?wsdl';
+$url = 'http://api.radioreference.com/soap2/?wsdl&v=latest';
 $client = new nusoap_client($url, 'WSDL');
 $p_post = $_POST;
-$result = $client->call('getCountryList', 
-  array(
-    'id'         =>  $p_post['id'], 
-    'nama'          =>  $p_post['nama'],
-    'code'           =>  $p_post['code'],
-    
-  )
-);
+$result = $client->call('getCountryList');
+echo'<table>';print_r($client->response); echo'</table>';
+
 ?>
